fd4e7e9bd51d7c4b7cc9aa65676669579182ad79
nascott <nascott94@outlook.com>
2022-04-13 12:09:02 -0600

    index and list added to components

---
+++ packages/my-first-theme/src/components/index.js
@@ -0,0 +1,34 @@
+// File: /packages/my-first-theme/src/components/index.js
+
+import React from "react";
+import { connect } from "frontity";
+import Link from "@frontity/components/link";
+import Switch from "@frontity/components/switch";
+import List from "./list";
+
+const Root = ({ state }) => {
+  const data = state.source.get(state.router.link);
+  return (
+    <>
+      <h1>Hello Frontity</h1>
+      <p>Current URL: {state.router.link}</p>
+      <nav>
+        <Link link="/">Home</Link>
+        <br />
+        <Link link="/page/2">More posts</Link>
+        <br />
+        <Link link="/about-us">About Us</Link>
+      </nav>
+      <hr />
+      <main>
+        <Switch>
+          <List when={data.isArchive} />
+          <div when={data.isPost}>This is a post</div>
+          <div when={data.isPage}>This is a page</div>
+        </Switch>
+      </main>
+    </>
+  );
+};
+
+export default connect(Root);
---
+++ packages/my-first-theme/src/index.js
@@ -0,0 +1,18 @@
+// File: /packages/my-first-theme/src/index.js
+
+import Root from "./components";
+
+const myFirstTheme = {
+  name: "my-first-theme",
+  roots: {
+    teleheathTheme: Root,
+  },
+  state: {
+    teleheathTheme: {},
+  },
+  actions: {
+    teleheathTheme: {},
+  },
+};
+
+export default myFirstTheme;
