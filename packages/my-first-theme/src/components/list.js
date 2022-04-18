e5a962686816711e00ec08bcc5f621e0e64ae71d
Dan Possehl <danielbpossehl@gmail.com>
2022-04-18 09:20:18 -0700

    styled

--- packages/my-first-theme/src/components/list.js
+++ packages/my-first-theme/src/components/list.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { connect } from "frontity";
+import { connect, styled } from "frontity";
 import Link from "@frontity/components/link";
+
+const Items = styled.div`
+  & > a {
+    display: block;
+    margin: 6px 0;
+    font-size: 1.2em;
+    color: steelblue;
+    text-decoration: none;
+  }
+`;
 
 const List = ({ state }) => {
   const data = state.source.get(state.router.link);
 
   return (
-    <div>
+    <Items>
       {data.items.map((item) => {
         const post = state.source[item.type][item.id];
         return (
@@ -16,7 +26,7 @@ const List = ({ state }) => {
           </Link>
         );
       })}
-    </div>
+    </Items>
   );
 };
 
